a11e4e8baff884262ab9a8e2a3c5a35ea5afb738
Harkelion <71925235+Harkelion@users.noreply.github.com>
2022-08-22 16:41:15 +0200

    Add files via upload

--- config/common.js
+++ config/common.js
@@ -1,38 +1,38 @@
 module.exports = [
-	// Enrage Added
-	{
-		type: 'added',
-		target: 'MyBoss',
-		abnormalities: 8888888,
-		message: 'Enrage {duration}'
-	},
-	// Enrage Expiring, notify at 12 and 6 seconds remaining
-	{
-		type: 'expiring',
-		target: 'MyBoss',
-		abnormalities: 8888888,
-		message: 'Enrage {duration}',
-		time_remaining: [12, 6]
-	},
-	// Enrage Removed, show next enrage %
-	{
-		type: 'removed',
-		target: 'MyBoss',
-		abnormalities: 8888888,
-		message: 'Enrage Ended - Next {nextEnrage}'
-	},
+  // Enrage Added
+  //   {
+  //     type: "Added",
+  //     target: "MyBoss",
+  //     abnormalities: 8888888,
+  //     message: "Enrage {duration}",
+  //   },
+  //   // Enrage Expiring, notify at 12 and 6 seconds remaining
+  //   {
+  //     type: "Expiring",
+  //     target: "MyBoss",
+  //     abnormalities: 8888888,
+  //     message: "Enrage {duration}",
+  //     time_remaining: [12, 6],
+  //   },
+  //   // Enrage Removed, show next enrage %
+  //   {
+  //     type: "Removed",
+  //     target: "MyBoss",
+  //     abnormalities: 8888888,
+  //     message: "Enrage Ended - Next {nextEnrage}",
+  //   },
 
-	// Contagion Added
-	{
-		type: 'added',
-		target: 'MyBoss',
-		abnormalities: [701700, 701701],
-		message: 'Contagion {duration}'
-	},
-/*
+  // Contagion Added
+  {
+    type: "Added",
+    target: "MyBoss",
+    abnormalities: [701700, 701701],
+    message: "Contagion {duration}",
+  },
+  /*
 	// Contagion Expiring, notify 6 seconds remaining
 	{
-		type: 'expiring',
+		type: 'Expiring',
 		target: 'MyBoss',
 		abnormalities: [701700, 701701],
 		message: 'Contagion {duration}',
@@ -40,24 +40,24 @@ module.exports = [
 	},
 	// Contagion Removed
 	{
-		type: 'removed',
+		type: 'Removed',
 		target: 'MyBoss',
 		abnormalities: [701700, 701701],
 		message: 'Contagion Ended'
 	},
 */
 
-	// Hurricane Added
-	{
-		type: 'added',
-		target: 'MyBoss',
-		abnormalities: 60010,
-		message: 'Hurricane {duration}'
-	},
-/*
+  // Hurricane Added
+  {
+    type: "Added",
+    target: "MyBoss",
+    abnormalities: 60010,
+    message: "Hurricane {duration}",
+  },
+  /*
 	// Hurricane Expiring, notify at 6 seconds remaining
 	{
-		type: 'expiring',
+		type: 'Expiring',
 		target: 'MyBoss',
 		abnormalities: 60010,
 		message: 'Hurricane {duration}',
@@ -65,24 +65,25 @@ module.exports = [
 	},
 	// Hurricane Removed
 	{
-		type: 'removed',
+		type: 'Removed',
 		target: 'MyBoss',
 		abnormalities: 60010,
 		message: 'Hurricane Ended'
 	},
 */
 
-	// Adrenaline Rush  Added
-	{
-		type: 'added',
-		target: 'Self',
-		abnormalities: [200701, 200700],
-		message: 'Adrenaline Rush {duration}'
-	},
+  // Adrenaline Rush  Added
+  {
+    type: "Added",
+    target: "Self",
+    abnormalities: [200701, 200700],
+    message: "Adrenaline Rush {duration}",
+  },
+  /*
 /*
 	// Adrenaline Rush  Expiring, notify at 6 seconds remaining
 	{
-		type: 'expiring',
+		type: 'Expiring',
 		target: 'Self',
 		abnormalities: [200701, 200700],
 		message: 'Adrenaline Rush {duration}',
@@ -90,59 +91,94 @@ module.exports = [
 	},
 	// Adrenaline Rush Removed
 	{
-		type: 'removed',
+		type: 'Removed',
 		target: 'Self',
 		abnormalities: [200701, 200700],
 		message: 'Adrenaline Rush Ended'
 	},
-	// Mystic Shield Added (Thrall)
-	{
-		type: 'added',
-		target: 'Self',
-		abnormalities: [702001],
-		message: 'Mysitc Shield up'
-	},
-	// Priest Shield Added (Kaia)
-	{
-		type: 'added',
-		target: 'Self',
-		abnormalities: [800300, 800302, 800303, 800304],
-		message: 'Priest Shield up.'
-	},
-	// Mystic Wrath Added
-	{
-		type: 'added',
-		target: 'Self',
-		abnormalities: [702004],
-		message: '{icon} {duration}'
-	},
-	// Priest Edict Added
-	{
-		type: 'added',
-		target: 'Self',
-		abnormalities: [805800],
-		message: '{icon} {duration}'
-	},
+	*/
+  // Mystic Shield Added (Thrall)
+  {
+    type: "Added",
+    target: "Self",
+    abnormalities: [702001, 701606],
+    message: "Mysitc Shield up",
+  },
+  // Mystic Shield (Thrall) Soon Expired
+  {
+    type: "Expiring",
+    target: "Self",
+    abnormalities: [702001, 701606],
+    message: "Mystic Shield Soon Expired {duration}",
+    time_remaining: 5,
+  },
+  // Mystic Shield (Thrall) Expired
+  {
+    type: "Removed",
+    target: "Self",
+    abnormalities: [702001, 701606],
+    message: "{red} Mystic Shield Expired {duration}",
+  },
+  // Priest Shield Added (Kaia)
+  {
+    type: "Added",
+    target: "Self",
+    abnormalities: [800300, 800302, 800303, 800304],
+    message: "Priest Shield up.",
+  },
+  // Priest Shield (Kaia) Soon Expired
+  {
+    type: "Expiring",
+    target: "Self",
+    abnormalities: [800300, 800302, 800303, 800304],
+    message: "Priest Shield Soon Expired {duration}",
+    time_remaining: 5,
+  },
+  //Priest Shield (Kaia) Expired
+  {
+    type: "Removed",
+    target: "Self",
+    abnormalities: [800300, 800302, 800303, 800304],
+    message: "{red} Priest Shield Expired {duration}",
+  },
+  // Mystic Wrath Added
+  {
+    type: "Added",
+    target: "Self",
+    abnormalities: [702004],
+    message: "{icon} {duration}",
+  },
+  // Priest Edict Added
+  {
+    type: "Added",
+    target: "Self",
+    abnormalities: [805800],
+    message: "{icon} {duration}",
+  },
 
-	//Pet buff
-	{
-		type: 'expiring',
-		target: 'Self',
-		abnormalities: [13000, 13001, 13002, 13003, 13004, 13005, 13006, 13007, 13008, 13009, 13010, 13011, 13012, 13013, 13014, 13015, 13016, 13017, 13018, 13019, 13020, 13021, 13022, 13023, 13024, 13025, 13026, 13027, 13028, 13029, 13030, 13031, 13032, 13033, 13034, 13035, 13036, 13037],
-		message: 'Pet bond skill is about to expire.',
-		time_remaining: 5
-	},
-*/
+  //Pet buff
+  {
+    type: "Expiring",
+    target: "Self",
+    abnormalities: [
+      13000, 13001, 13002, 13003, 13004, 13005, 13006, 13007, 13008, 13009,
+      13010, 13011, 13012, 13013, 13014, 13015, 13016, 13017, 13018, 13019,
+      13020, 13021, 13022, 13023, 13024, 13025, 13026, 13027, 13028, 13029,
+      13030, 13031, 13032, 13033, 13034, 13035, 13036, 13037,
+    ],
+    message: "Pet bond skill is about to expire.",
+    time_remaining: 5,
+  },
 
-	// Missing Battle Solution / Nostrum
-	{
-		type: 'MissingDuringCombat',
-		target: 'Self',
-		abnormalities: [4030, 4031, 4020, 4021],
-		message: 'Missing {icon}',
-		rewarn_timeout: 15
-	},
-/*
+  // Missing Battle Solution / Nostrum
+  {
+    type: "MissingDuringCombat",
+    target: "Self",
+    abnormalities: [4030, 4031, 4020, 4021],
+    message: "Missing {icon}",
+    rewarn_timeout: 15,
+  },
+  /*
 	// Vergos Aggro Debuff
    {
 		type: 'AddedOrRefreshed',
@@ -162,7 +198,7 @@ module.exports = [
 
 	// Bahaar Laser
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'PartyIncludingSelf',
 		abnormalities: 90442502,
 		message: 'LASER on {name}!'
@@ -170,7 +206,7 @@ module.exports = [
 
 	// SoH; Sea Stun
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'PartyIncludingSelf',
 		abnormalities: 30209101,
 		message: 'Stun DoT inc on {name}!'
@@ -178,7 +214,7 @@ module.exports = [
 
 	// SoH; Sea Fear
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'PartyIncludingSelf',
 		abnormalities: 30209102,
 		message: 'Fear wave inc on {name}!'
@@ -186,7 +222,7 @@ module.exports = [
 
 	// CKG; Garden Target
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'PartyIncludingSelf',
 		abnormalities: 32060024,
 		message: '{icon} on {name}'
@@ -194,7 +230,7 @@ module.exports = [
 
 	// CKG; Lumikan Immunity
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'Self',
 		abnormalities: [31040003, 32040003],
 		message: '{icon} {duration}'
@@ -202,10 +238,10 @@ module.exports = [
 
 	// CKG; Lumikan HM Target
 	{
-		type: 'added',
+		type: 'Added',
 		target: 'PartyIncludingSelf',
 		abnormalities: 32040007,
 		message: '{icon} on {name}'
 	},
 */
-]
+];
--- config/valkyrie.js
+++ config/valkyrie.js
@@ -1,1 +1,23 @@
-module.exports = []
+module.exports = [
+  {
+    type: "Expiring",
+    target: "Self",
+    abnormalities: 10155130,
+    message: "{red} Ragnarok soon end {duration}",
+    time_remaining: 5,
+  },
+  {
+    type: "Expiring",
+    target: "Self",
+    abnormalities: 10155250,
+    message: "{red} Balder's Tears soon end {duration}",
+    time_remaining: 3,
+  },
+  {
+    type: "Removed",
+    target: "Self",
+    abnormalities: 10155250,
+    message: "{red} Balder's Tears end",
+    time_remaining: 3,
+  },
+];
